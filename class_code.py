3316beeed5cff1bab87029560a343d7559d9d920
Elfego <156902688+ePinonIV@users.noreply.github.com>
2024-11-21 14:31:07 -0700

    Update class_code.py

--- class_code.py
+++ class_code.py
@@ -20,7 +20,7 @@ st.title("Water Supply Data Visualization App")
 #if uploaded_file is not None:
 if 1 == 1:
     # Read the CSV file
-    data = pd.read_csv('NewMexico_Data_V3.csv')
+    data = pd.read_csv('NewMexico_Data_V4.csv')
     #st.write("### Data Preview")
     st.markdown('<p style="color:blue; font-size:30px;">Data Preview.</p>', unsafe_allow_html=True)
 
